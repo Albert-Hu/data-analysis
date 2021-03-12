26453f201a987db26ac6cb7db21316dd2fe29b4c
Albert <0x.albert@gmail.com>
2021-03-12 18:05:47 +0800

    add html parsing.

--- 台灣股市/query_dividend.py
+++ 台灣股市/query_dividend.py
@@ -11,7 +11,8 @@ from selenium.webdriver.firefox.options import Options
 
 from bs4 import BeautifulSoup
 
-import argparse
+import argparse, time
+import pandas as pd
 
 def run(stock_code):
   opts = Options()
@@ -19,17 +20,19 @@ def run(stock_code):
   url = "https://mops.twse.com.tw/mops/web/t05st09_2"
   #This example requires Selenium WebDriver 3.13 or newer
   with webdriver.Chrome(options=opts, executable_path="./drivers/geckodriver") as driver:
-    wait = WebDriverWait(driver, 10)
     driver.get(url)
     Select(driver.find_element(By.ID, "isnew")).select_by_value("false")
     driver.find_element(By.ID, "co_id").send_keys(stock_code + Keys.RETURN)
     driver.find_element(By.ID, "date1").send_keys("100" + Keys.RETURN)
     driver.find_element(By.ID, "date2").send_keys("110" + Keys.RETURN)
-    # driver.find_element_by_css_selector("input[id='qryType'][value='2']").click()
-    driver.find_element_by_css_selector("input[type='button'][value=' 查詢 ']").click()
-    print(driver.page_source)
-    #parser = BeautifulSoup(driver.page_source, 'html.parser')
-    #print(parser.title)
+    driver.find_element(By.CSS_SELECTOR, "input[type='button'][value=' 查詢 ']").click()
+    time.sleep(1)
+    html = BeautifulSoup(driver.page_source, "html.parser")
+    for row in html.find_all('tr', attrs={"class": "odd"}):
+      columns = row.find_all('td')
+      columns = columns[4:5] + columns[6:18]
+      columns = list(map(lambda c: ''.join(c.get_text().split()), columns))
+      print(columns)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
